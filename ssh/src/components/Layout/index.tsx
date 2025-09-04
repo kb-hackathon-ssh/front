95d3a2c6e36d91926741d75bff0e8bd5dc5e90b5
smilevictory <leevictory7272@gmail.com>
2025-09-04 13:53:12 +0900

    feat: 레이아웃 푸터 생성

--- ssh/src/components/Layout/index.tsx
+++ ssh/src/components/Layout/index.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Layout/Header";
+import Footer from "@/components/Layout/Footer";
 
 export default function RootLayout() {
   return (
     <>
       <Header />
-      <main className="mx-auto w-full max-w-screen-xl px-4 py-6">
+      <main className="mx-auto w-full max-w-screen-xl px-4 pt-20 pb-6 flex-1">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
