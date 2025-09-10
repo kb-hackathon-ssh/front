42e93321a4f515c8633879080074a4c1e590b037
kxxheehxxn <fpqk7099@gmail.com>
2025-09-10 22:03:10 +0900

    fix: vercel

--- ssh/vite.config.ts
+++ ssh/vite.config.ts
@@ -1,6 +1,7 @@
+/// <reference types="node" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
